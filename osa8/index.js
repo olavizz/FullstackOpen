e7fd15a3bc5b783c6df94744ac2a1cbd3f254288
Olavi Suojala <o.suojala@gmail.com>
2025-11-23 22:35:10 +0200

    8.5

--- osa8/index.js
+++ osa8/index.js
@@ -98,7 +98,7 @@ const typeDefs = `
   type Query {
     bookCount: Int,
     authorCount: Int,
-    allBooks(author: String): [Book],
+    allBooks(author: String, genre: String): [Book],
     allAuthors: [Authors]
   }
   
@@ -123,12 +123,23 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
+      if (args.author && args.genre) {
+        let authorbooks = books.filter(book => book.author === args.author)
+        return authorbooks.filter(book => book.genres.includes(args.genre))
+      }
       if (args.author) {
         return books.filter(book => book.author === args.author)
-      } else {
+
+      } 
+      if (args.genre) {
+        return books.filter(book => book.genres.includes(args.genre))
+      }
+
+      else {
         return books
       }
     },
+    
     allAuthors: () => authors
   },
 
